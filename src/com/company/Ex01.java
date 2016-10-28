f26fc70dc3dce9c0351377cfd6683cab126a0726
Enrique Imbert <e-imbert@hotmail.com>
2016-10-28 19:25:19 +0200

    Nuevo ejercicio

--- src/com/company/Ex01.java
+++ src/com/company/Ex01.java
@@ -3,6 +3,18 @@ package com.company;
 public class Ex01 {
 
     public static void main(String[] args) {
-	// write your code here
+        int cont = 2;
+
+        int[] Array = new int[20];
+        for (int i = 0; i < Array.length; i++) {
+
+            Array[i] = cont;
+            cont += 2;
+        }
+
+        //printar el array
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i]);
+        }
     }
 }
