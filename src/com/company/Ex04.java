f4e55cfce9c91736c52aa3cb14ca6361080a99e2
Enrique Imbert <e-imbert@hotmail.com>
2016-11-03 17:18:17 +0100

    Nuevo ejercicio

---
+++ src/com/company/Ex04.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by 25374183P on 03/11/2016.
+ */
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Cuantas alturas deseas introducir?");
+            int num= scanner.nextInt();
+        System.out.println("Dame "+num+" Alturas");
+    }
+}
---
+++ src/com/company/Ex05.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by 25374183P on 03/11/2016.
+ */
+public class Ex05 {
+}
